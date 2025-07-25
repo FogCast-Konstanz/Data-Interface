c84916e5f1cd18bf1d16abbde1e5ec310cd515be
ElijahSt199 <elijah.stauss@gmail.com>
2025-07-25 10:53:36 +0200

    updated benchmarking query

--- backend/services/benchmarking/influx.py
+++ backend/services/benchmarking/influx.py
@@ -4,20 +4,30 @@ from config import influx_client, INFLUXDB_ORG
 
 BUCKET = "WeatherForecast"
 
-def query_benchmark_scores(time_range: str):
-    query = f'''from(bucket: "WeatherForecast") 
-        |> range(start: -{time_range})
-        |> filter(fn: (r) => r["_measurement"] == "error_score")
-        |> filter(fn: (r) => r["_field"] == "error")
-        |> keep(columns: ["_time", "_model", "feature", "_value", "forecast_horizon"])
-        '''
+def get_latest_benchmark():
     query_api = influx_client.query_api()
-    tables = query_api.query(query=query, org=INFLUXDB_ORG)
 
-    data = []
-    for table in tables:
-        for record in table.records:
-            data.append(record.values)
+    # Step 1: Fetch ALL rows in the last day
+    raw_query = f'''
+    from(bucket: "benchmark_score")
+    |> range(start: -1d)
+    |> filter(fn: (r) => r["_measurement"] == "forecast_error")
+    |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
+    |> keep(columns: ["model", "cloud_cover", "dew_point_2m", "precipitation", 
+                        "relative_humidity_2m", "surface_pressure", "temperature_2m", 
+                        "lead_time", "forecast_date"])
+    '''
 
-    df = pd.DataFrame(data)    
-    return df
+    df_all = query_api.query_data_frame(raw_query)
+
+    if isinstance(df_all, list):
+        df_all = pd.concat(df_all)
+    df_all = df_all.reset_index(drop=True)
+
+    # Get the latest timestamp
+    latest_forecast_datetime = df_all["forecast_date"].max()
+
+    # Filter only rows with that latest timestamp
+    df_latest = df_all[df_all["forecast_date"] == latest_forecast_datetime]
+
+    return df_latest
